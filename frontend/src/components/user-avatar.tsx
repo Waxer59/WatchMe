14b0fe6aba201b8d535d38ff97313ebda933d06e
Marcos Corrochano Arroyo <83825951+Hyro32@users.noreply.github.com>
2024-12-21 11:45:08 +0100

    feat: basic ui

---
+++ frontend/src/components/user-avatar.tsx
@@ -0,0 +1,9 @@
+import { Avatar, Button } from "@chakra-ui/react";
+
+export function UserAvatar() {
+  return (
+    <Button variant="plain" padding="unset">
+      <Avatar size="sm" name="Waxer59" />
+    </Button>
+  )
+}
