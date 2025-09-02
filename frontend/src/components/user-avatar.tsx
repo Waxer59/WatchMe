abe6d0c195cb2e3c5d68edc4be6ab6bfc7363222
Hugo Boyano <hnboyano@gmail.com>
2025-09-03 00:50:31 +0200

    add small fixes

--- frontend/src/components/user-avatar.tsx
+++ frontend/src/components/user-avatar.tsx
@@ -2,9 +2,9 @@ import { Avatar, Button } from '@chakra-ui/react'
 
 export function UserAvatar() {
   return (
-    <Button variant="plain" padding="unset">
-      <Avatar.Root>
-        <Avatar.Fallback name="Waxer59" className='shadow-xl' />
+    <Button variant="plain">
+      <Avatar.Root className='bg-secondary!'>
+        <Avatar.Fallback name="Waxer59" />
       </Avatar.Root>
     </Button>
   )
