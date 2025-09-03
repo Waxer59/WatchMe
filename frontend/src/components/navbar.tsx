ad30df3055a99464a15f5e3311e73774d120e016
Hugo Boyano <hnboyano@gmail.com>
2025-09-03 22:37:46 +0200

    add favicon

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -13,22 +13,20 @@ export function Navbar() {
       alignItems="center"
       justifyContent="space-between"
       padding="4">
-      <Box as="div" display="flex" alignItems="center" gap="6">
+      <Box as="div" display="flex" alignItems="center" gap="12">
         <NextLink href="/" className="text-2xl uppercase">
           <h2>
             <strong>WATCHME</strong>
           </h2>
         </NextLink>
-        <NextLink href="/following" className="font-semibold text-sm">
-          Following
-        </NextLink>
-        <NextLink href="/discover" className="font-semibold text-sm">
-          Discover
-        </NextLink>
+        <NextLink href="/following" className='rounded-md text-sm hover:text-white hover:bg-gray-700 transition-all h-9 px-4 py-2'>Following</NextLink>
+        <NextLink href="/discover" className='rounded-md text-sm hover:text-white hover:bg-gray-700 transition-all h-9 px-4 py-2'>Discover</NextLink>
       </Box>
       <SearchInput />
       <Box as="div" display="flex" alignItems="center" gap="2">
-        <Button variant="subtle" colorPalette="gray" rounded="lg">Log in</Button>
+        <Button variant="subtle" colorPalette="gray" rounded="lg">
+          Log in
+        </Button>
         <UserAvatar />
       </Box>
     </Box>
